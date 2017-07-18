bb32a443471ae853e92a21a3b67bb8c45dbe48f9
gdiazh <g.hernan.diaz@gmail.com>
2017-07-17 22:02:16 -0400

    non ros interface working on mega ADK

--- arduino/tactil_driver/tactil_driver.ino
+++ arduino/tactil_driver/tactil_driver.ino
@@ -1,26 +1,26 @@
-// #define LOGGER_MIN_SEVERITY LOGGER_SEVERITY_NONE
-#define LOGGER_MIN_SEVERITY LOGGER_SEVERITY_DEBUG
+#define LOGGER_MIN_SEVERITY LOGGER_SEVERITY_NONE
+// #define LOGGER_MIN_SEVERITY LOGGER_SEVERITY_DEBUG
 // https://github.com/rorromr/serial_dxl/archive/v0.1.tar.gz
 #include <SerialDXL.h>
 
 // SERVO DXL basic config
-#define SERVO_MODEL 100
-#define SERVO_FIRMWARE 100
+#define HAND_MODEL 100
+#define HAND_FIRMWARE 100
 
 /**
- * DEVICEDXL_MIN_BUFFER = 6
- * Is the overhead for dynamixel like devices 
  * Has MODEL_L, MODEL_H, FIRMWARE, ID, BAUDRATE and RETURN DELAY
  * This variables are inside DeviceDXL class
  */ 
 
-// We add 13 UInt8 (13 bytes)
-#define SERVO_MMAP_SIZE 2
+// We add 3 UInt8 (3 bytes)
+#define HAND_MMAP_SIZE 3
 
 // MMap position for commands (mem Addrs)
 
-#define TACTIL1_ADDR      6
-#define TACTIL2_ADDR      7
+#define TACTIL1_ADDR_LB      6
+#define TACTIL1_ADDR_HB      7
+#define TACTIL2_ADDR_LB      8
+#define TACTIL2_ADDR_HB      9
 
 //commands (not addr)
 #define CMD1 1
@@ -34,8 +34,8 @@
 #define PIN_TACTIL2 A1
 
 // Com pins
-#define PIN_DIR 3
-#define PIN_RST 2
+#define PIN_DIR 4
+#define PIN_RST 8
 
 /**
  * @brief Tactil sensor Data Interface using DXL communication protocol over RS485.
@@ -44,25 +44,26 @@
  * @param dir_pin Toggle communication pin.
  * @param reset_pin Pin for reset device.
  */
-class HandDXL: public DeviceDXL<SERVO_MODEL, SERVO_FIRMWARE, SERVO_MMAP_SIZE>
+class HandDXL: public DeviceDXL<HAND_MODEL, HAND_FIRMWARE, HAND_MMAP_SIZE>
 {
   public:
-    HandDXL(uint8_t dir_pin, uint8_t reset_pin, uint8_t num_sensors, uint8_t sensors_pins[]):
+    HandDXL(uint8_t dir_pin, uint8_t reset_pin):
     DeviceDXL(), // Call parent constructor
     dir_pin_(dir_pin),    // Direction pin for RS485
     reset_pin_(reset_pin), // Reset pin
-    num_sensors_(num_sensors), // numero de servos
     tactil1_(MMap::Access::RW, MMap::Storage::RAM),
     tactil2_(MMap::Access::RW, MMap::Storage::RAM),
     cmd1_(MMap::Access::RW, MMap::Storage::RAM)
     {
       // Config pins
       pinMode(dir_pin_, OUTPUT);
-      pinMode(reset_pin_, OUTPUT);
-      for(uint8_t i=0;i<num_sensors_;i++)
-      {
-        pinMode(sensors_pins[i], INPUT);
-      }
+      pinMode(reset_pin_, INPUT);
+      pinMode(PIN_TACTIL1, INPUT);
+      pinMode(PIN_TACTIL2, INPUT);
+
+      // Get mask and port for data control pin
+      dataControlPinMask_ = digitalPinToBitMask(dir_pin);
+      dataControlPinReg_ = portOutputRegister(digitalPinToPort(dir_pin));
     }
 
     void init()
@@ -83,6 +84,9 @@ class HandDXL: public DeviceDXL<SERVO_MODEL, SERVO_FIRMWARE, SERVO_MMAP_SIZE>
         */
         DEBUG_PRINTLN("Load default");
         mmap_.load(); // Load values from EEPROM
+        DEBUG_PRINT("tactil1: ");DEBUG_PRINTLN(tactil1_.data);
+        DEBUG_PRINT("tactil2: ");DEBUG_PRINTLN(tactil1_.data);
+        INFO_PRINT("id: ");INFO_PRINTLN_RAW(id_.data);
 
         /*
         * Read sensor data
@@ -103,7 +107,9 @@ class HandDXL: public DeviceDXL<SERVO_MODEL, SERVO_FIRMWARE, SERVO_MMAP_SIZE>
     void update()
     {
       read_tactil1();
+      delayMicroseconds(5);
       read_tactil2();
+      Serial.print(tactil1_.data);Serial.print("\t\t");Serial.println(tactil2_.data);
     }
 
     inline bool onReset()
@@ -114,43 +120,53 @@ class HandDXL: public DeviceDXL<SERVO_MODEL, SERVO_FIRMWARE, SERVO_MMAP_SIZE>
 
     inline void setTX()
     {
-      digitalWrite(dir_pin_,HIGH);
+      *dataControlPinReg_ |= dataControlPinMask_;
     }
 
     inline void setRX()
     {
-      digitalWrite(dir_pin_,LOW);
+      *dataControlPinReg_ &= ~dataControlPinMask_;
     }
 
   private:
+    // Communication direction pin
+    uint8_t dataControlPinMask_;
+    volatile uint8_t *dataControlPinReg_;
+
     const uint8_t dir_pin_;     // Toggle communication direction pin
     const uint8_t reset_pin_;   // Reset pin
-    const uint8_t num_sensors_;
     
     // sensors variables
-    MMap::Integer<UInt8, 0U, 255U, 0U>::type tactil1_;
-    MMap::Integer<UInt8, 0U, 255U, 0U>::type tactil2_;
+    MMap::Integer<UInt16, 0U, 1023U, 0U>::type tactil1_;
+    MMap::Integer<UInt16, 0U, 1023U, 0U>::type tactil2_;
     MMap::Integer<UInt8, 0U, 255U, 0U>::type cmd1_;
 };
 
-uint8_t sensors_pins[] = {PIN_TACTIL1, PIN_TACTIL2};
-
 //General device
-HandDXL hand_dxl(PIN_DIR, PIN_RST, NUM_SENSORS, sensors_pins);
+HandDXL hand_dxl(PIN_DIR, PIN_RST);
 SerialDXL<HandDXL> serialDxl;
 
 void setup()
 {
+     //Serial port for debug
+    Serial.begin(115200);
+    DEBUG_PRINTLN("INIT SETUP");
+
+    hand_dxl.init();
+    hand_dxl.reset();
+    hand_dxl.mmap_.serialize();
+
     // Init serial communication using Dynamixel format
-    serialDxl.init(&Serial3 , &hand_dxl);
+    serialDxl.init(&Serial1 , &hand_dxl);
+    DEBUG_PRINTLN("INIT MAIN PROGRAM");
 }
 
 void loop()
 {
     // Update msg buffer
-    while (Serial3.available())
+    while (Serial1.available())
     {
-        serialDxl.process(Serial3.read());
+        serialDxl.process(Serial1.read());
     }
 
     hand_dxl.mmap_.deserialize();
--- interface/hand_interface.py
+++ interface/hand_interface.py
@@ -13,8 +13,10 @@ IMPORTANT: Modify only if you are sure of hardware specifications. See Documenta
 
 # MMap position for commands (mem Addrs)
 
-TACTIL1_ADDR = 6
-TACTIL2_ADDR = 7
+TACTIL1_ADDR_LB = 6
+TACTIL1_ADDR_HB = 7
+TACTIL2_ADDR_LB = 8
+TACTIL2_ADDR_HB = 9
 
 # Sensor commands (not addr)
 CMD1 = 1
@@ -24,7 +26,7 @@ class HandInterface(object):
     def __init__(self, dxl_io, dev_id = 1):
         self.dxl = dxl_io
         self.id = dev_id
-        self.state = [0]
+        self.state = 0
 
     def write_addr(self, addr, value):
         result = []
@@ -49,8 +51,8 @@ class HandInterface(object):
         except Exception as e:
             rospy.logwarn('Exception thrown while writing addres %d' % (state_variable))
             # return e
-            self.state = result[5]
-        return self.state
+            return -1
+        return result[5]
 
     def change_id(self, new_id):
         if new_id<32:
@@ -59,21 +61,27 @@ class HandInterface(object):
         self.write_addr(3, new_id)
 
     def read_tactil_sensor(self, id):
+        value_lb = 0
+        value_hb = 0
         if id == 1:
-            return self.get_state(TACTIL1_ADDR)
+            value_lb = self.get_state(TACTIL1_ADDR_LB)
+            value_hb = self.get_state(TACTIL1_ADDR_HB)
         elif id == 2:
-            return self.get_state(TACTIL2_ADDR)
+            value_lb = self.get_state(TACTIL2_ADDR_LB)
+            value_hb = self.get_state(TACTIL2_ADDR_HB)
         else:
             print 'Not Allow ID:%d. Must be 1 or 2' % (id)
+        return value_lb | (value_hb<<8)
 
 if __name__ == '__main__':
     DEV_ID = 36
-    dxl = DynamixelIO('/dev/ttyUSB0', baudrate = 200000)
+    dxl = DynamixelIO('/dev/ttyUSB0', baudrate = 1000000)
     hand = HandInterface(dxl, dev_id = DEV_ID)
-    print hand.ping()
+    # print dxl.ping(DEV_ID)
+    # hand.change_id(36)
 
     while True:
         t1 = hand.read_tactil_sensor(1)
         t2 = hand.read_tactil_sensor(2)
-        print 'Tactil sensor 1:%d' %(t1)
-        print 'Tactil sensor 2:%d' %(t2)
+        print 'Tactil sensor L:%d \t\t\t Tactil sensor R:%d' %(t1, t2)
+        time.sleep(0.1)
